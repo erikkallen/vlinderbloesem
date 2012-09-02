a30ceb0bbd43da116f9fdfc1c46a1892951674b3
Erik Kallen <erikkallen@Eriks-oude-laptop.local>
2012-09-02 13:42:32 +0200

    security fix

--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -1,5 +1,5 @@
 class FotosController < ApplicationController
-  before_filter :authenticate_user!, :except => :index
+  before_filter :authenticate_user!, :except => [:index, :show]
   before_filter :get_fotos, :only => [:show, :index]
   # GET /fotos
   # GET /fotos.json
