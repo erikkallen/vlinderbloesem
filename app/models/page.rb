911f92fbe60d75b76b7b12885ceb230b7ad84f10
Erik Kallen <info@erikkallen.nl>
2012-09-02 14:01:13 +0200

    photo upload fix

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,3 @@
 class Page < ActiveRecord::Base
-  has_attached_file :foto,  :styles => { :medium => "213" }, :processors => [:thumbnail,:rounded_corners]
+  has_attached_file :foto,  :styles => { :medium => "213" }, :processors => [:thumbnail]
 end
